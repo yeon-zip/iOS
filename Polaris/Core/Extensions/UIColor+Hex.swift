39ab64c84082dd1107df873f15902cb0dff38da7
Ryu Tae Hyun <ryu200112@gmail.com>
2026-04-08 11:09:26 +0900

    feat: introduce app design tokens

---
+++ Polaris/Core/Extensions/UIColor+Hex.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Hex.swift
+//  Polaris
+//
+//  Created by Codex on 4/8/26.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: UInt, alpha: CGFloat = 1) {
+        let red = CGFloat((hex >> 16) & 0xFF) / 255
+        let green = CGFloat((hex >> 8) & 0xFF) / 255
+        let blue = CGFloat(hex & 0xFF) / 255
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
